df39037a1d328f158032b5b930cdaf79da2cf28b
Yuri Mansani Vieira <yurimv9@gmail.com>
2026-04-18 14:48:12 -0300

    Criando abstracao do methodo index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
+
 abstract class Controller
 {
-    //
+
+    public function index()
+    {
+        $classname = class_basename(static::class);
+
+        $base = str_replace('Controller', '', $classname);
+        
+        return view('pages.' . Str::plural(Str::kebab($base)) . '.index');
+    }
 }
