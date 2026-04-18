b03c89f706b5fdda5a40416bf84e844ef32fc5cc
Yuri Mansani Vieira <yurimv9@gmail.com>
2026-04-18 15:30:02 -0300

    Criando models abstratas

---
+++ app/Models/Abstract/BaseAuthenticableModel.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Models\Abstract;
+
+use Illuminate\Database\Eloquent\Model;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+abstract class BaseAuthenticableModel extends Authenticatable
+{
+    protected static array $fields = [];
+
+    public static function fields(): array
+    {
+        return static::$fields;
+    }
+
+    public function getFillable()
+    {
+        return array_keys(static::$fields);
+    }
+}
---
+++ app/Models/Abstract/BaseModel.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Models\Abstract;
+
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    protected static array $fields = [];
+
+    public static function fields(): array
+    {
+        return static::$fields;
+    }
+
+    public function getFillable()
+    {
+        return array_keys(static::$fields);
+    }
+}
