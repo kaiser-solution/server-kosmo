368fa00e249e30a01ba2489e224164aad54d9110
Yuri Mansani Vieira <yurimv9@gmail.com>
2026-04-18 14:45:52 -0300

    Criando as factories

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Application;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Application>
+ */
+class ApplicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'namespace' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'endpoint' => $this->faker->url(),
+        ];
+    }
+}
---
+++ database/factories/PlanFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Plan;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Plan>
+ */
+class PlanFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2),
+            'currency' => $this->faker->randomElement(['BRL','USD'])
+        
+        ];
+    }
+}
