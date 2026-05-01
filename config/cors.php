b6fa9b85e18246010ff42ffb001d69e0f833c935
Yuri Mansani Vieira <yurimv9@gmail.com>
2026-05-01 10:00:00 -0300

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://front.kaisersolution.com.br', '*'],
+    'allowed_origins' => ['https://front.kaisersolution.com.br',],
 
     'allowed_origins_patterns' => [],
 
